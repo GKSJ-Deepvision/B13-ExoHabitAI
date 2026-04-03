f94bf00090d74d783eb2d618c0208465db276723
SAKSHI GUPTA <113574159+Sakshi-gupta66@users.noreply.github.com>
2026-04-03 18:09:58 +0530

    --

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 from flask_cors import CORS
-
+import os
 
 
 app = Flask(__name__)
@@ -119,4 +119,4 @@ def rank():
 
 # ------------------ RUN ------------------
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
